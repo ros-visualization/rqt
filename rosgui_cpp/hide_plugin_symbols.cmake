a74f5ba0ac2950806131319445f8e3855c560b8a
Dirk Thomas <web@dirk-thomas.net>
2011-12-21 22:30:34 +0100

    fixed test for version-script support to hide plugin symbols

--- rosgui_cpp/hide_plugin_symbols.cmake
+++ rosgui_cpp/hide_plugin_symbols.cmake
@@ -1,7 +1,7 @@
 # hides the symbols of a Poco plugin
 macro(rosgui_cpp__hide_plugin_symbols package_name class_name)
   set(_version_script version__${package_name}__${class_name}.script)
-  FILE(WRITE ${CMAKE_BINARY_DIR}/${_version_script}
+  file(WRITE "${CMAKE_BINARY_DIR}/${_version_script}"
     "    {
       global:
         pocoBuildManifest;
@@ -14,9 +14,9 @@ macro(rosgui_cpp__hide_plugin_symbols package_name class_name)
   )
   # checks if the linker supports version script
   include(${CMAKE_ROOT}/Modules/TestCXXAcceptsFlag.cmake)
-  check_cxx_accepts_flag("-Wl,--version-script,${_version_script}" LD_ACCEPTS_VERSION_SCRIPT)
+  check_cxx_accepts_flag("-Wl,--version-script,\"${CMAKE_BINARY_DIR}/${_version_script}\"" LD_ACCEPTS_VERSION_SCRIPT)
   if(LD_ACCEPTS_VERSION_SCRIPT)
-    set(CMAKE_SHARED_LINKER_FLAGS "${CMAKE_SHARED_LINKER_FLAGS} -Wl,-version-script=${CMAKE_BINARY_DIR}/${_version_script}")
-  endif(LD_ACCEPTS_VERSION_SCRIPT)
+    set(CMAKE_SHARED_LINKER_FLAGS "${CMAKE_SHARED_LINKER_FLAGS} -Wl,-version-script=\"${CMAKE_BINARY_DIR}/${_version_script}\"")
+  endif()
 endmacro()
 
