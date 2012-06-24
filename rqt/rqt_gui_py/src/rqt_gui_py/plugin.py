f8dc402afb659c17dbe94c8e4f9c042bbb5d923f
Dirk Thomas <dthomas@willowgarage.com>
2012-06-24 16:10:30 -0500

    added explicit rqt plugin class (for API doc only)

---
+++ rqt/rqt_gui_py/src/rqt_gui_py/plugin.py
@@ -0,0 +1,82 @@
+# Software License Agreement (BSD License)
+#
+# Copyright (c) 2012, Willow Garage, Inc.
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#
+#  * Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+#  * Redistributions in binary form must reproduce the above
+#    copyright notice, this list of conditions and the following
+#    disclaimer in the documentation and/or other materials provided
+#    with the distribution.
+#  * Neither the name of Willow Garage, Inc. nor the names of its
+#    contributors may be used to endorse or promote products derived
+#    from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+# COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
+import roslib
+roslib.load_manifest('rqt_gui_py')
+
+import qt_gui.plugin.Plugin as Base
+
+class Plugin(Base):
+
+    """
+    Interface for Python plugins which use the ROS client library.
+    User-defined plugins may either subclass `rqt_gui_py.plugin.Plugin` or according to duck typing implement only the needed methods.
+    A plugin must not call rospy.init_node() as this is performed once by the framework.
+    The name of the ROS node consists of the prefix "rqt_gui_py_node_" and the process id.
+    """
+
+    def __init__(self, context):
+        super(Plugin, self).__init__(context)
+
+    def shutdown_plugin(self):
+        """
+        Shutdown and clean up the plugin before unloading.
+        I.e. unregister subscribers and stop timers.
+        """
+        pass
+
+    def save_settings(self, plugin_settings, instance_settings):
+        """
+        Save the intrinsic state of the plugin to the plugin-specific or instance-specific `Settings`.
+        @param plugin_settings: the plugin-specific settings
+        @type plugin_settings: qt_gui.settings.Settings
+        @param instance_settings: the instance-specific settings
+        @type instance_settings: qt_gui.settings.Settings
+        """
+        pass
+
+    def restore_settings(self, plugin_settings, instance_settings):
+        """
+        Restore the intrinsic state of the plugin from the plugin-specific or instance-specific `Settings`.
+        @param plugin_settings: the plugin-specific settings
+        @type plugin_settings: qt_gui.settings.Settings
+        @param instance_settings: the instance-specific settings
+        @type instance_settings: qt_gui.settings.Settings
+        """
+        pass
+
+    #def trigger_configuration(self):
+        #"""
+        #Trigger a configuration request from the title bar of one of the dock widgets.
+        #If this method is available the `DockWidgetTitleBar` will show the configuration action.
+        #"""
+        #pass
