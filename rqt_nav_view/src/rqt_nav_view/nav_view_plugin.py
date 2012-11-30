f1a271537036bca9058091b5a420921d6e23e401
Aaron Blasdel <ablasdel@willowgarage.com>
2012-11-30 14:53:32 -0800

    rqt_nav_view: Added Exception and explaination of the limitation of one plugin per rqt instance

--- rqt_nav_view/src/rqt_nav_view/nav_view_plugin.py
+++ rqt_nav_view/src/rqt_nav_view/nav_view_plugin.py
@@ -7,6 +7,8 @@ from qt_gui.plugin import Plugin
 class NavViewPlugin(Plugin):
     def __init__(self, context):
         super(NavViewPlugin, self).__init__(context)
+        if context.serial_number() > 1:
+            raise RuntimeError("Due to a limitation of tf_frames you may not run more than one instance of rqt_nav_view.")
         self._widget = NavViewWidget()
         if context.serial_number() > 1:
             self._widget.setWindowTitle(self._widget.windowTitle() + (' (%d)' % context.serial_number()))
