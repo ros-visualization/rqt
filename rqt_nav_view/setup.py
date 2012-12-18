a33608a66ca53340f66d84bf3f3a376b416195cf
Aaron Blasdel <ablasdel@willowgarage.com>
2012-12-17 20:21:17 -0800

    catkinize: added setup.py files

--- rqt_nav_view/setup.py
+++ rqt_nav_view/setup.py
@@ -1,22 +1,11 @@
-#1/usr/bin/env python
-from setuptools import setup
-import sys
-sys.path.insert(0, 'src')
+#!/usr/bin/env python
 
-setup(name='nav_view',
-      version= '0.1.0',
-      packages=['nav_view'],
-      package_dir = {'':'src'},
-      install_requires=[],
-      author = "Ze'ev Klapow", 
-      author_email = "zklapow@willowgarage.com",
-      url = "",
-      download_url = "", 
-      keywords = [],
-      classifiers = [
-        "Programming Language :: Python", 
-        "License :: OSI Approved :: BSD License" ],
-      description = "",
-      long_description = "",
-      license = "BSD"
-      )
+from distutils.core import setup
+from catkin_pkg.python_setup import generate_distutils_setup
+
+d = generate_distutils_setup(
+    packages=['rqt_nav_view'],
+    package_dir={'': 'src'}
+)
+
+setup(**d)
