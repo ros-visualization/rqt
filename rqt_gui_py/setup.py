93a34c6f530d4919bc2a0383d2e2ba0e51ae9ea8
Stephen <brawner@gmail.com>
2018-10-30 16:09:00 -0700

    Porting to ros2

--- rqt_gui_py/setup.py
+++ rqt_gui_py/setup.py
@@ -4,9 +4,16 @@ from distutils.core import setup
 
 from catkin_pkg.python_setup import generate_distutils_setup
 
+package_name = 'rqt_gui_py'
+
 d = generate_distutils_setup(
     packages=['rqt_gui_py'],
-    package_dir={'': 'src'}
+    package_dir={'': 'src'},
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
 )
 
 setup(**d)
