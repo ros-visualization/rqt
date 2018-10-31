2159e16bc15b5f26983a36d34596b89edf5bdc7f
Stephen <brawner@gmail.com>
2018-10-31 12:52:38 -0700

    Typo

--- rqt_gui/src/rqt_gui/rospkg_plugin_provider.py
+++ rqt_gui/src/rqt_gui/rospkg_plugin_provider.py
@@ -58,7 +58,7 @@ class RospkgPluginProvider(RosPluginProvider):
             for package_name, package_path in get_packages_with_prefixes().items():
                 package_file_path = os.path.join(
                     package_path, 'share', package_name, PACKAGE_MANIFEST_FILENAME)
-                package_share_path = os.join(package_path, 'share')
+                package_share_path = os.path.join(package_path, 'share')
                 if os.path.isfile(package_file_path):
                     # only try to import catkin if a PACKAGE_FILE is found
                     try:
