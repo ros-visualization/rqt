ceba98793e6d3973987fcecee71f89a91ebcfba9
Dirk Thomas <dirk-thomas@users.noreply.github.com>
2018-12-11 15:13:33 -0800

    fix install destination of plugin.xml (#179)

--- rqt_gui_py/setup.py
+++ rqt_gui_py/setup.py
@@ -10,7 +10,7 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        ('share/' + package_name + '/resource/', ['plugin.xml'])
+        ('share/' + package_name, ['plugin.xml'])
     ],
     install_requires=['setuptools'],
     zip_safe=False,
