8aaf266f4ff62f4bd3b4ee310104bc8dcd9b7d61
Stephen <brawner@gmail.com>
2018-12-20 17:29:55 -0800

    Adding logic for actions

--- rqt_py_common/src/rqt_py_common/message_helpers.py
+++ rqt_py_common/src/rqt_py_common/message_helpers.py
@@ -30,6 +30,7 @@
 
 # Author: Michael Lautman
 import importlib
+from collections import defaultdict, namedtuple
 
 from ament_index_python import get_resource
 from ament_index_python import get_resources
@@ -41,6 +42,118 @@ MSG_MODE = 'msg'
 SRV_MODE = 'srv'
 ACTION_MODE = 'action'
 
+ROSIDL_FILTERS = {
+    MSG_MODE: lambda n: n.endswith('.msg'),
+    SRV_MODE: lambda n: n.endswith('.srv'),
+    ACTION_MODE: lambda n: n.endswith('.action')
+}
+
+# Currently there is no high-level action class. This namedtuple will serve as a packed
+# representation of an Action
+ActionTuple = namedtuple('Action', ['Goal', 'Result', 'Feedback'])
+
+
+def _strip_type_name(type_name):
+    prefixes = ['msg/', 'srv/', 'action/']
+    suffixes = ['.msg', '.srv', '.action']
+    for prefix, suffix in zip(prefixes, suffixes):
+        found_match = False
+        if type_name.startswith(prefix):
+            type_name = type_name[len(prefix):]
+            found_match = True
+        if type_name.endswith(suffix):
+            type_name = type_name[:len(type_name) - len(suffix)]
+            found_match = True
+        if found_match:
+            break
+    return type_name
+
+
+def _filter_rosidl_types(rosidl_type, interface_names):
+    """
+    Filter a list of rosidl interfaces with a specific type.
+
+    :param rosidl_type: rosidl type to filter by, one of ['msg', 'srv', 'action']
+    :type rosidl_type: str
+    :param interface_names: list of interface names return from ament_index_python.get_resource
+    :type interface_names: list(str)
+    :returns: a list of the filtered rosidl types
+    """
+    if rosidl_type not in ROSIDL_FILTERS:
+        raise ValueError('Invalid rosidl_type type "{}". Needs to be one of {}'.format(
+            rosidl_type, ROSIDL_FILTERS.keys()))
+
+    filter_fn = ROSIDL_FILTERS[rosidl_type]
+    filtered = filter(filter_fn, interface_names)
+    stripped = map(_strip_type_name, filtered)
+    return list(stripped)
+
+
+def get_rosidl_types_of_type(package_name, rosidl_type):
+    """
+    Retrieve a list of rosidl interfaces of a specific type provided by a ROS2 package.
+
+    :param package_name: ROS2 package name
+    :type package_name: str
+    :param rosidl_type: rosidl type to filter by, one of ['msg', 'srv', 'action']
+    :type rosidl_type: str
+    :returns: a list of the filtered rosidl types
+    """
+    try:
+        content, _ = get_resource('rosidl_interfaces', package_name)
+    except LookupError:
+        return []
+    interface_names = content.splitlines()
+    return _filter_rosidl_types(rosidl_type, interface_names)
+
+
+def get_rosidl_types(package_name):
+    """
+    Retrieve a dictionary of lists for all rosidl interface types provided by a ROS2 package.
+
+    :param package_name: ROS2 package name
+    :type package_name: str
+    :returns: a dictionary mapping rosidl type to a list {'msg': [...], 'srv': ... }
+    """
+    rosidl_types = {}
+    try:
+        content, _ = get_resource('rosidl_interfaces', package_name)
+    except LookupError:
+        return []
+    interface_names = content.splitlines()
+    for filter_type in ROSIDL_FILTERS.keys():
+        rosidl_types[filter_type] = _filter_rosidl_types(filter_type, interface_names)
+    return rosidl_types
+
+
+def get_all_rosidl_types():
+    """
+    Retrieve a dictionary mapping packages to their provided rosidl interfaces.
+
+    :returns: a dictionary mapping packages to their types
+    """
+    all_rosidl_types = {}
+    for package_name in get_resources('rosidl_interfaces').keys():
+        all_rosidl_types[package_name] = get_rosidl_types(package_name)
+    return all_rosidl_types
+
+
+def get_all_rosidl_types_of_type(rosidl_type):
+    """
+    Retrieve a dictionary mapping packages to a specific rosidl type.
+
+    :returns: a dictionary mapping packages to the rosidl interfaces of a specific type
+    """
+    if rosidl_type not in ROSIDL_FILTERS:
+        raise ValueError('Invalid rosidl_type type "{}". Needs to be one of {}'.format(
+            filter_type, ROSIDL_FILTERS.keys()))
+
+    all_rosidl_types = get_all_rosidl_types()
+    return {
+        package_name:
+            d[rosidl_type] for package_name, d in all_rosidl_types.items() if d[rosidl_type]
+    }
+
 
 def get_all_service_types():
     """
@@ -48,12 +161,7 @@ def get_all_service_types():
 
     :returns: a dictionary of the form {'package_name', ['srv1', 'srv2', ...]}
     """
-    all_service_types = {}
-    for package_name in get_resources('rosidl_interfaces').keys():
-        service_types = get_service_types(package_name)
-        if service_types:
-            all_service_types[package_name] = service_types
-    return all_service_types
+    return get_all_rosidl_types_of_type(SRV_MODE)
 
 
 def get_service_types(package_name):
@@ -63,16 +171,7 @@ def get_service_types(package_name):
     :param package_name: a string eg 'std_srvs'
     :returns: a dictionary of the form {'package_name', ['srv1', 'srv2', ...]}
     """
-    try:
-        content, _ = get_resource('rosidl_interfaces', package_name)
-    except LookupError:
-        return []
-    interface_names = content.splitlines()
-    # TODO(dirk-thomas) this logic should come from a rosidl related package
-    # Only return services in srv folder
-    all_srvs = [
-        n[4:-4] for n in interface_names if n.startswith('srv/') and n.endswith('.srv')]
-    return all_srvs
+    return get_rosidl_types_of_type(package_name, SRV_MODE)
 
 
 def get_all_message_types():
@@ -81,12 +180,7 @@ def get_all_message_types():
 
     :returns: a dictionary of the form {'package_name', ['msg1', 'msg2', ...]}
     """
-    all_message_types = {}
-    for package_name in get_resources('rosidl_interfaces').keys():
-        message_types = get_message_types(package_name)
-        if message_types:
-            all_message_types[package_name] = message_types
-    return all_message_types
+    return get_all_rosidl_types_of_type(MSG_MODE)
 
 
 def get_message_types(package_name):
@@ -96,19 +190,29 @@ def get_message_types(package_name):
     :param package_name: a string eg 'std_msgs'
     :returns: a dictionary of the form {'std_msgs', ['Bool', 'String', ...]}
     """
-    try:
-        content, _ = get_resource('rosidl_interfaces', package_name)
-    except LookupError:
-        return []
-    interface_names = content.splitlines()
-    # TODO(dirk-thomas) this logic should come from a rosidl related package
-    # Only return messages in msg folder
-    all_msgs = [
-        n[4:-4] for n in interface_names if n.startswith('msg/') and n.endswith('.msg')]
-    return all_msgs
+    return get_rosidl_types_of_type(package_name, MSG_MODE)
 
 
-def _get_message_service_class_helper(message_type, mode, logger=None):  # noqa: C901
+def get_all_action_types():
+    """
+    Get all the packages that support actions and their action types.
+
+    :returns: a dictionary of the form {'package_name', ['action1', 'action2', ...]}
+    """
+    return get_all_rosidl_types_of_type(ACTION_MODE)
+
+
+def get_action_types(package_name):
+    """
+    Find all actions available in the package.
+
+    :param package_name: a string eg 'test_msgs'
+    :returns: a list of action names ['Fibonacci', ...]
+    """
+    return get_rosidl_types_of_type(package_name, ACTION_MODE)
+
+
+def _get_rosidl_class_helper(message_type, mode, logger=None):  # noqa: C901
     """
     A helper function for common logic to be used by get_message_class and get_service_class.
 
@@ -124,7 +228,7 @@ def _get_message_service_class_helper(message_type, mode, logger=None):  # noqa:
     if logger is None:
         logger = logging.get_logger('_get_message_service_class_helper')
 
-    if not (mode == MSG_MODE or mode == SRV_MODE):
+    if mode not in ROSIDL_FILTERS.keys():
         logger.warn('invalid mode {}'.format(mode))
         return None
 
@@ -136,7 +240,10 @@ def _get_message_service_class_helper(message_type, mode, logger=None):  # noqa:
     package = message_info[0]
     base_type = message_info[1]
 
-    _, resource_path = get_resource('rosidl_interfaces', package)
+    try:
+        _, resource_path = get_resource('rosidl_interfaces', package)
+    except LookupError:
+        return None
     python_pkg = None
     class_val = None
 
@@ -172,7 +279,7 @@ def get_service_class(srv_type):
         return _srv_class_cache[srv_type]
 
     logger = logging.get_logger('get_service_class')
-    class_val = _get_message_service_class_helper(srv_type, SRV_MODE, logger)
+    class_val = _get_rosidl_class_helper(srv_type, SRV_MODE, logger)
 
     if class_val is not None:
         _srv_class_cache[srv_type] = class_val
@@ -197,12 +304,40 @@ def get_message_class(message_type):
 
     logger = logging.get_logger('get_message_class')
 
-    class_val = _get_message_service_class_helper(message_type, MSG_MODE, logger)
+    class_val = _get_rosidl_class_helper(message_type, MSG_MODE, logger)
 
     if class_val is not None:
         _message_class_cache[message_type] = class_val
 
     return class_val
+
+
+_action_class_cache = {}
+
+
+def get_action_class(action_type):
+    """
+    Gets the action class from a string representation.
+
+    :param action_type: the type of action in the form `action_pkg/Action`
+    :type action_type: str
+
+    :returns: None or the Class
+    """
+    if action_type in _action_class_cache:
+        return _action_class_cache[action_type]
+
+    logger = logging.get_logger('get_action_class')
+
+    # There is no high level action class. Import Goal, Result, Feedback separately
+    goal_val = _get_rosidl_class_helper(action_type + '_Goal', ACTION_MODE, logger)
+    result_val = _get_rosidl_class_helper(action_type + '_Result', ACTION_MODE, logger)
+    feedback_val = _get_rosidl_class_helper(action_type + '_Feedback', ACTION_MODE, logger)
+    action_val = ActionTuple(goal_val, result_val, feedback_val)
+    if goal_val is not None and result_val is not None and feedback_val is not None:
+        _action_class_cache[action_type] = action_val
+
+    return action_val
 
 
 def get_message_text_from_class(msg_class):
@@ -225,3 +360,22 @@ def get_service_text_from_class(srv_class):
         '{0} {1}\n'.format(slot_type, slot_name) for
         slot_name, slot_type in srv_slot_dict_res.items()])
     return ''.join(srv_txt)
+
+
+def get_action_text_from_class(action_class):
+    """Get a string representation of the message class."""
+    action_slot_dict_goal = action_class.Goal.get_fields_and_field_types()
+    action_slot_dict_res = action_class.Result.get_fields_and_field_types()
+    action_slot_dict_feedback = action_class.Feedback.get_fields_and_field_types()
+    action_txt = [
+        '{0} {1}\n'.format(slot_type, slot_name) for
+        slot_name, slot_type in action_slot_dict_goal.items()]
+    action_txt.extend(['---\n'])
+    action_txt.extend([
+        '{0} {1}\n'.format(slot_type, slot_name) for
+        slot_name, slot_type in action_slot_dict_res.items()])
+    action_txt.extend(['---\n'])
+    action_txt.extend([
+        '{0} {1}\n'.format(slot_type, slot_name) for
+        slot_name, slot_type in action_slot_dict_feedback.items()])
+    return ''.join(action_txt)
