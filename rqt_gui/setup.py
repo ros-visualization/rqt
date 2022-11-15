e59d4e2ca528f926cb6621b97a6fd2ab0b16f8c1
Audrow Nash <audrow@openrobotics.org>
2022-11-15 15:25:47 -0600

    Update maintainers to Dharini Dutia
    Signed-off-by: Audrow Nash <audrow@openrobotics.org>

--- rqt_gui/setup.py
+++ rqt_gui/setup.py
@@ -21,8 +21,8 @@ setup(
     zip_safe=True,
     author='Dirk Thomas',
     author_email='dthomas@osrfoundation.org',
-    maintainer='Dirk Thomas',
-    maintainer_email='dthomas@osrfoundation.org',
+    maintainer='Dharini Dutia',
+    maintainer_email='dharini@openrobotics.org',
     keywords=['ROS'],
     classifiers=[
         'Intended Audience :: Developers',
