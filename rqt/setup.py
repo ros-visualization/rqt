aaf58066ed9eba5c71911a88e0c860b40768901a
Alejandro Hernandez Cordero <ahcorde@gmail.com>
2026-06-18 13:15:41 +0200

    2.0.1

--- rqt/setup.py
+++ rqt/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 package_name = 'rqt'
 setup(
     name=package_name,
-    version='2.0.0',
+    version='2.0.1',
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
