bea8f65f92bc522e11dca223a56791f275fab50a
Alejandro Hernandez Cordero <ahcorde@gmail.com>
2026-07-10 15:41:46 +0200

    Review feedback
    Signed-off-by: Alejandro Hernandez Cordero <ahcorde@gmail.com>

--- rqt_gui_cpp/src/rqt_gui_cpp/nodelet_plugin_provider.cpp
+++ rqt_gui_cpp/src/rqt_gui_cpp/nodelet_plugin_provider.cpp
@@ -104,7 +104,7 @@ std::shared_ptr<Plugin> NodeletPluginProvider::create_plugin(
   std::shared_ptr<rqt_gui_cpp::Plugin> instance =
     qt_gui_cpp::RosPluginlibPluginProvider<rqt_gui_cpp::Plugin>::create_plugin(lookup_name);
   instance->passInNode(node_);
-  instances_[&*instance] = nodelet_name.c_str();
+  instances_[instance.get()] = nodelet_name.c_str();
 
   return instance;
 }
